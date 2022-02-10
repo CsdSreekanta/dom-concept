b2dbd96af1c0a0e736c769bd31750240b9c026d8
CsdSreekanta <sreekanta.csd@gmail.com>
2022-02-10 17:59:40 +0600

    shadow added

--- project.js
+++ project.js
@@ -14,5 +14,18 @@ document.getElementById('add-btn').addEventListener('click', function(){
        <td><button class="btn btn-danger delete-btn">Delete</button><button class="btn btn-success done-btn">Done</button></td>`;
        mainContainer.appendChild(tableContainer);
        document.getElementById('input-value').value= '';
+
+      const deleteButton= document.getElementsByClassName('delete-btn')
+     for(const button of deleteButton ){
+         button.addEventListener('click', function(e){
+            e.target.parentNode.parentNode.style.display="none"
+         })
+     }
+       const doneButton= document.getElementsByClassName('done-btn');
+       for(const button of doneButton){
+           button.addEventListener('click', function(e){
+               e.target.parentNode.parentNode.style.textDecoration ="line-through";
+           })
+       }
     }
 })
